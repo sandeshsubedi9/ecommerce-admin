07fb9b68ce7587a4a0565034ca2e765771382182
Sandesh <sandeshsubedi2020@gmail.com>
2025-07-22 14:42:03 +0545

    Updated Feature

--- components/overview.tsx
+++ components/overview.tsx
@@ -11,7 +11,7 @@ export const Overview: React.FC<OverviewProps> = ({ data }) => {
         <ResponsiveContainer width="100%" height={350}>
             <BarChart data={data}>
                 <XAxis dataKey="name" stroke='#888888' fontSize={12} tickLine={false} axisLine={false} />
-                <YAxis stroke='#888888' fontSize={12} tickLine={false} axisLine={false} tickFormatter={(value) => `$${value}`}/>
+                <YAxis stroke='#888888' fontSize={12} tickLine={false} axisLine={false} tickFormatter={(value) => `Rs.${value}`}/>
                 <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
             </BarChart>
         </ResponsiveContainer>
